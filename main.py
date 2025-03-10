0e159ada85de507ed67301c6074e26b98c409d68
= <=>
2025-03-10 15:36:00 -0300

    Create letter for each person listed

--- main.py
+++ main.py
@@ -9,4 +9,6 @@ with open("./Input/Letters/starting_letter.txt") as letter_file:
     for name in names:
         stripped_name = name.strip()
         new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
-        print(new_letter)
+        with open(f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", mode="w") as completed_letter:
+            completed_letter.write(new_letter)
+
