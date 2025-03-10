fcf6ce670f56c81c0a4a82832a3cfedb13737650
= <=>
2025-03-10 15:33:11 -0300

    Replace the placeholder with the names on the file

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+PLACEHOLDER = "[name]"
+
 with open("./Input/Names/invited_names.txt") as names_file:
     names = names_file.readlines()
     print(names)
+
+with open("./Input/Letters/starting_letter.txt") as letter_file:
+    letter_contents = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letter_contents.replace(PLACEHOLDER, stripped_name)
+        print(new_letter)
